979c7c1da8972e632cc1aeae965bfe43898b7a28
ifat <ifat802@gmail.com>
2023-04-08 14:17:51 +0600

    added mongodb session storage

--- web/shopify.js
+++ web/shopify.js
@@ -1,9 +1,9 @@
 import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
-import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
+import { MongoDBSessionStorage } from "@shopify/shopify-app-session-storage-mongodb";
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-01";
 
-const DB_PATH = `${process.cwd()}/database.sqlite`;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://base:basepassword@localhost:27017/base";
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
@@ -30,7 +30,7 @@ const shopify = shopifyApp({
     path: "/api/webhooks",
   },
   // This should be replaced with your preferred storage strategy
-  sessionStorage: new SQLiteSessionStorage(DB_PATH),
+  sessionStorage: new MongoDBSessionStorage(MONGODB_URI),
 });
 
 export default shopify;
